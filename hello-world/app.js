b4c9f4b7af90b37cf04082ff64bb985eec37d815
Phoenix Zhang <phoenix@linkpower.com>
2020-10-21 12:26:54 +0800

    change the hello world string

--- hello-world/app.js
+++ hello-world/app.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = module.exports = new Koa();
 
 app.use(async function(ctx) {
-  ctx.body = 'Hello World';
+  ctx.body = 'Hello Phoenix, this is your first Koa project, good luck!';
 });
 
 if (!module.parent) app.listen(3000);
